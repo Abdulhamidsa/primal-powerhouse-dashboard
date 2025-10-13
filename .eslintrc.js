e97f448832c1f8f98a5664c0ea53c5bb7756a8c2
Abdulhamidsa <alsaati.abdulhamid@gmail.com>
2025-10-13 13:58:06 +0200

    style: Standardize quotation marks in ESLint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,13 @@
 module.exports = {
-  extends: ["next/core-web-vitals"],
+  extends: ['next/core-web-vitals'],
   rules: {
     // Disable problematic rules for deployment
-    "@typescript-eslint/no-explicit-any": "warn",
-    "@typescript-eslint/no-unused-vars": "warn",
-    "@typescript-eslint/ban-ts-comment": "warn",
-    "@next/next/no-img-element": "warn",
-    "@next/next/no-html-link-for-pages": "warn",
-    "react/no-unescaped-entities": "warn",
-    "react-hooks/exhaustive-deps": "warn",
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-unused-vars': 'warn',
+    '@typescript-eslint/ban-ts-comment': 'warn',
+    '@next/next/no-img-element': 'warn',
+    '@next/next/no-html-link-for-pages': 'warn',
+    'react/no-unescaped-entities': 'warn',
+    'react-hooks/exhaustive-deps': 'warn',
   },
 };
